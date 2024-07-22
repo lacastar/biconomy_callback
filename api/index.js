5e7ea864cb8c2c53c6bb9f6b58787dac396c33b7
Laszlo Szenthe <lacus-github@rainstorm.org>
2024-07-22 20:30:23 +0200

    logic fix

--- api/index.js
+++ api/index.js
@@ -6,7 +6,8 @@ app.use(express.json());
 app.post("/", (req, res) => {
   const data = req.body;
   console.log("data", data);
-  const { sender } = data.data;
+  const { userOp } = data.data;
+  const { sender } = userOp
 
   if (sender === "0x47Bf99e56F29b29c5604EB0567144b2121cB891b") {
     res.json({
