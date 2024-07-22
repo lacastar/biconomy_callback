67e78ae7e681398e8ff490101d376450fc304786
Laszlo Szenthe <lacus-github@rainstorm.org>
2024-07-22 20:10:01 +0200

    logic

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  const data = req.body;
+  console.log("data", data);
+  const { sender } = data.data;
+
+  if (sender === "0x47Bf99e56F29b29c5604EB0567144b2121cB891b") {
+    res.json({
+      arePoliciesVerified: true,
+    });
+  } else {
+    res.json({
+      arePoliciesVerified: false,
+    });
+  }
+});
+
+app.listen(8080, () => console.log("Server listening on port 8080!"));
